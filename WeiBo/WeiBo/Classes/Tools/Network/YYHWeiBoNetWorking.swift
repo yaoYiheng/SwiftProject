94239b3850c2f1e2cfe3dbe36d601523e1dcbd4d
YihengYao <fn1117@qq.com>
2017-07-26 23:47:34 +1200

    17_07_26_创建网络工具单例
        ///是一个保存在静态区的常量, 执行闭包
        ///在第一次执行该代码时, 执行闭包, 并将结果赋值给shared
        static let shared = YYHWeiBoNetWorking()

--- WeiBo/WeiBo/Classes/Tools/Network/YYHWeiBoNetWorking.swift
+++ WeiBo/WeiBo/Classes/Tools/Network/YYHWeiBoNetWorking.swift
@@ -11,5 +11,8 @@ import AFNetworking
 
 class YYHWeiBoNetWorking: AFHTTPSessionManager {
 
+    ///是一个保存在静态区的常量, 执行闭包
+    ///在第一次执行该代码时, 执行闭包, 并将结果赋值给shared
+    static let shared = YYHWeiBoNetWorking()
     
 }
