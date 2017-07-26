e9c9260934edf6fe270825b93cc2508465830b9d
YihengYao <fn1117@qq.com>
2017-07-26 23:43:50 +1200

    17_07_26_瞎折腾

---
+++ WeiBo/WeiBo/Classes/Tools/Network/YYHWeiBoNetWorking.swift
@@ -0,0 +1,15 @@
+//
+//  YYHWeiBoNetWorking.swift
+//  WeiBo
+//
+//  Created by 姚懿恒 on 2017/7/26.
+//  Copyright © 2017年 姚懿恒. All rights reserved.
+//
+
+import UIKit
+import AFNetworking
+
+class YYHWeiBoNetWorking: AFHTTPSessionManager {
+
+    
+}
