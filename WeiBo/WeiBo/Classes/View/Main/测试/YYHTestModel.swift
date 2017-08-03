320aa6a0413bbbbeff99fb81db314c5fc52f050a
YihengYao <fn1117@qq.com>
2017-08-03 23:24:42 +1200

    ???

---
+++ WeiBo/WeiBo/Classes/View/Main/测试/YYHTestModel.swift
@@ -0,0 +1,20 @@
+//
+//  YYHTestModel.swift
+//  WeiBo
+//
+//  Created by 姚懿恒 on 2017/8/3.
+//  Copyright © 2017年 姚懿恒. All rights reserved.
+//
+
+import Foundation
+import YYModel
+class YYHTestModel: NSObject, YYModel {
+    var biz_feature: Int = 0
+    var in_reply_to_status_id: Int = 0
+    var reposts_count:Int = 0
+    var source_allowclick:Int = 0
+
+
+
+
+}
